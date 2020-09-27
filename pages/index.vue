b89e4f93b56a3373e0e127f244dd3019e15236a2
garunitule <youtaiguhe@gmail.com>
2020-09-28 00:26:13 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+      <div v-html="$md.render(post)"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: '# Goの変数宣言\n解説します。\n```go\nvar name = "go"\n```\n![]()\n'
+    }
+  },
+  mounted() {
+    Prism.highlightAll()
+  },
+}
+</script>
---
+++ plugins/prism.js
@@ -0,0 +1,10 @@
+import Prism from 'prismjs'
+
+// テーマの追加
+import 'prism-themes/themes/prism-nord.css'
+
+// 言語別にシンタックスハイライトを追加
+import 'prismjs/components/prism-go'
+// ...
+
+export default Prism
